729ecabb59f6af1c1d7132ee3005f6f0a8fded7b
abjia <daohao1914>
2015-08-26 14:01:24 +0800

    验证模式

--- config.js
+++ config.js
@@ -2,15 +2,30 @@
  * mysql 配置
  */
 var mysql =  {
+    "host": "127.0.0.1",
+    "port": "6379",
+    "ttl": 60 * 60 * 24 * 30,   //Session的有效期为30天 单位秒
+    //disableTTL  禁用设置的 TTL
+    //db         使用第几个数据库
+    //pass       Redis数据库的密码
+    //prefix     数据表前辍即schema, 默认为 "sess:"
+    prefix : "common:",
+    db     : 2
 
 }
 
 var redis =  {
-    port: 6379,          // Redis port
-    host: '127.0.0.1',   // Redis host
-    family: 4,           // 4 (IPv4) or 6 (IPv6)
-    password: 'auth',
-    db: 0
+    "host": "10.10.68.15",
+    "port": "6379",
+    "pass" : "zhiliao",
+    "ttl": 60 * 60 * 24 * 30,   //Session的有效期为30天 单位秒
+    //disableTTL  禁用设置的 TTL
+    //db         使用第几个数据库
+    //pass       Redis数据库的密码
+    //prefix     数据表前辍即schema, 默认为 "sess:"
+    prefix : "common:",
+    db     : 1
+
 }
 
 
--- routes/route.js
+++ routes/route.js
@@ -4,23 +4,18 @@
  * @type {*|exports}
  */
 
-var jwt =  require('express-jwt');
-
 var  routes =
     {
-        "/auth" : require("./auth/auth.ctrl"),
+
         "/movie": require("./movie/index.ctrl"),
         "/"     : require("./main.ctrl")
     }
-
 
 var init  = function(app){
     console.log("初始化路由....");
     for(var r  in routes){
         app.use(r,routes[r]);
     }
-
-
 }
 
 exports.init = init;
